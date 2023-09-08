3f85229ace0fc9bd9cee7c7d380c3e936a486172
Emre Özbağdatlı <74432853+EmreOzbagdatli@users.noreply.github.com>
2023-09-08 19:25:51 +0300

    Initial commit

---
+++ UnsplashApp/UnsplashAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashAppApp.swift
+//  UnsplashApp
+//
+//  Created by Emre Özbağdatlı on 4.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
