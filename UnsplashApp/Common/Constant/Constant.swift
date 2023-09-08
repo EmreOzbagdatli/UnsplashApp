5ade1992c36b80599f61caf24eef1f47559f2bce
Emre Özbağdatlı <74432853+EmreOzbagdatli@users.noreply.github.com>
2023-09-08 19:35:57 +0300

    Folders created

---
+++ UnsplashApp/Common/Constant/Constant.swift
@@ -0,0 +1,8 @@
+//
+//  Constant.swift
+//  UnsplashApp
+//
+//  Created by Emre Özbağdatlı on 8.09.2023.
+//
+
+import Foundation
---
+++ UnsplashApp/Scene/Favourite/VİEW/FavouriteView.swift
@@ -0,0 +1,20 @@
+//
+//  FavouriteView.swift
+//  UnsplashApp
+//
+//  Created by Emre Özbağdatlı on 8.09.2023.
+//
+
+import SwiftUI
+
+struct FavouriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavouriteView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavouriteView()
+    }
+}
